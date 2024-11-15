089dabd3ed88ff0f0cd9c43e4685c57652b096b5
utkarshmisra16 <109430518+utkarshmisra16@users.noreply.github.com>
2024-11-15 10:32:17 +0530

    Time: 2 ms (100%), Space: 43.7 MB (16.39%) - LeetHub

--- 1547-destination-city/1547-destination-city.java
+++ 1547-destination-city/1547-destination-city.java
@@ -1,14 +1,14 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-       HashMap<String, Integer> mp = new HashMap<>(); 
+       Set<String> mp = new HashSet<>(); 
        for(List<String> path : paths){
             String source = path.get(0);
-            mp.put(source, mp.getOrDefault(source, 0) + 1);
+            mp.add(source);
         }
 
         for(List<String> path : paths){
             String dest = path.get(1);
-            if(mp.getOrDefault(dest, 0) == 0){
+            if(!mp.contains(dest)){
                 return dest;
             }
         }
