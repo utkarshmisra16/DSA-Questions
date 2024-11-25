427c242059c54419264e8605453bb71e3e258621
utkarshmisra16 <109430518+utkarshmisra16@users.noreply.github.com>
2024-11-25 12:10:21 +0530

    Time: 2 ms (98.23%), Space: 43.7 MB (58.71%) - LeetHub

--- 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.java
+++ 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.java
@@ -1,15 +1,26 @@
 class Solution {
     public boolean makeEqual(String[] words) {
-        Map <Character, Integer> mp = new HashMap<>();
+        int arr[] = new int[26];
         for(String word : words){
             for(char ch : word.toCharArray()){
-                mp.put(ch, mp.getOrDefault(ch, 0)+1);
+                arr[ch-'a']++;
             }
-        }
-        for(int value : mp.values()){
-            if(value%words.length != 0)
+        }  
+        for(int i=0; i<26; i++){
+            if(arr[i]%words.length != 0)
                 return false;
         }
+
+        // Map <Character, Integer> mp = new HashMap<>();
+        // for(String word : words){
+        //     for(char ch : word.toCharArray()){
+        //         mp.put(ch, mp.getOrDefault(ch, 0)+1);
+        //     }
+        // }
+        // for(int value : mp.values()){
+        //     if(value%words.length != 0)
+        //         return false;
+        // }
         return true;
     }
 }
