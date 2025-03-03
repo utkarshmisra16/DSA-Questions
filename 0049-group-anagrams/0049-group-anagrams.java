0ab68e15b7ea3217443664fb30682129a541d40b
Utkarsh Misra <109430518+utkarshmisra16@users.noreply.github.com>
2025-03-03 11:16:21 +0530

    Time: 6 ms (97.94%), Space: 47.7 MB (81.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,31 +5,14 @@ class Solution {
             return new ArrayList<>();
         HashMap<String, List<String>> Map = new HashMap<>();
         for(String s : strs){
-            String freq = frequency(s);
-            if(Map.containsKey(freq)){
-                Map.get(freq).add(s);
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            if(!Map.containsKey(key)){
+                Map.put(key, new ArrayList<>());
             }
-            else{
-                ArrayList<String> list = new ArrayList<>();
-                list.add(s);
-                Map.put(freq, list);
-            }
+            Map.get(key).add(s);
         }
         return new ArrayList<>(Map.values());
-    }
-
-    public String frequency(String strs){
-        int arr[] = new int[26];
-        for(char ch : strs.toCharArray()){
-            arr[ch - 'a']++;
-        }
-        StringBuilder sb = new StringBuilder("");
-        char ch = 'a';
-        for(int i : arr){
-            sb.append(ch);
-            sb.append(i);
-            ch++;
-        }
-        return sb.toString();
     }
 }
